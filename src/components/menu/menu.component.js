50a8904dddce56759e29c3ad6058b83a1afe20f1
Adrián Gómez Llorente <adgllorente@gmail.com>
2017-04-22 21:41:10 +0200

    [[FEAT]] Add printing option

--- src/components/menu/menu.component.js
+++ src/components/menu/menu.component.js
@@ -1,5 +1,27 @@
 import menuView from './menu.view.html';
+import angular from 'angular';
 
 export default {
-  templateUrl: menuView
+  templateUrl: menuView,
+  controller: menuController
 };
+
+menuController.$inject = ['$window'];
+
+/**
+ * Menu controller.
+ *
+ * @param {any} $window
+ */
+function menuController($window) {
+  var ctrl = this;
+
+  /**
+   * Callback executed to print the page.
+   */
+  ctrl.onPrint = () => {
+    // Remove output scroll.
+    document.getElementsByClassName('paw-main--output')[0].scrollTop = 0;
+    $window.print();
+  };
+}
